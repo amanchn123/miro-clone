87b57d6c31a430536d62be570b0b01bf84049329
amanchn123 <118670647+amanchn123@users.noreply.github.com>
2024-02-27 22:51:29 +0530

    ppp

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://just-mastodon-0.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ]
