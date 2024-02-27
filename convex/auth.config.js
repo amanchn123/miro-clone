bca604b49b76b75875de25b136d74864d7841c43
amanchn123 <118670647+amanchn123@users.noreply.github.com>
2024-02-27 23:18:31 +0530

    ttttp

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://wonderful-scorpion-283.convex.cloud",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ]
